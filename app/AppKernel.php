4d24e08f85e8edcd7c8324ae54403a70e1ac29e1
Златко Христов <about@z-latko.info>
2014-08-29 01:30:39 +0300

    Adding sonata stuff

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -16,6 +16,16 @@ class AppKernel extends Kernel
             new Symfony\Bundle\AsseticBundle\AsseticBundle(),
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
+            new FOS\UserBundle\FOSUserBundle(),
+            new Sonata\UserBundle\SonataUserBundle('FOSUserBundle'),
+            new Application\Sonata\UserBundle\ApplicationSonataUserBundle(),
+            new Sonata\AdminBundle\SonataAdminBundle(),
+            new Sonata\EasyExtendsBundle\SonataEasyExtendsBundle(),
+            new Sonata\DoctrineORMAdminBundle\SonataDoctrineORMAdminBundle(),
+            new Sonata\CoreBundle\SonataCoreBundle(),
+            new Sonata\BlockBundle\SonataBlockBundle(),
+            new Knp\Bundle\MenuBundle\KnpMenuBundle(),
+            new Application\MainBundle\ApplicationMainBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
---
+++ src/Application/MainBundle/ApplicationMainBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Application\MainBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class ApplicationMainBundle extends Bundle
+{
+}
