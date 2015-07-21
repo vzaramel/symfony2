4c82c49958aa323c73b9e3d601974daa6146d93f
vzaramel <vinicius.zaramella@gmail.com>
2015-07-20 22:22:13 -0300

    test github hook

--- src/Application/MainBundle/Controller/DefaultController.php
+++ src/Application/MainBundle/Controller/DefaultController.php
@@ -9,8 +9,6 @@ class DefaultController extends Controller
     public function indexAction($name)
     {
         var_dump($name);
-
-        echo "2";
         return $this->render('ApplicationMainBundle:Default:index.html.twig', array('name' => $name));
     }
 }
